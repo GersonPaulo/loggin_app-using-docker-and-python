e25be5618b93b8503ced31b028421a1fabfd110a
GersonPaulo <igorpaulo64@gmail.com>
2025-05-26 00:39:28 +0100

    primeiro file apenas para o lgoin --1

---
+++ login.py
@@ -0,0 +1,35 @@
+class Login :
+
+    def __init__(self):
+        self.users = dict()
+        self.u_mail = str()
+        self.u_passe = str()
+
+
+    def add_mail(self):
+        temp_var = str(input('insira o seu email:>'))
+        while "@" not in temp_var:
+            print('E-mail invalido')
+            temp_var = input('insira o seu email:>')
+        if "@" in temp_var:
+            self.u_mail = temp_var
+            return  self.u_mail
+
+    def add_pase(self):
+        temp_var = str(input('insira o sua password min 8 digitos:>'))
+        while (len(temp_var)) <= 7 :
+            print('password invalida minimo 8 digitos')
+            temp_var = str(input('digite password:>'))
+        if len(temp_var) >= 8:
+            self.u_passe = temp_var
+            return self.u_passe
+
+
+
+
+
+
+
+
+
+
